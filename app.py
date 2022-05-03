492cff3c9188d911abf4db128efbc51ef9e7d9de
alexsandro-matias <matiasalexsandro@gmail.com>
2022-05-03 19:01:53 -0300

    adicionado servidor web básico para armazenar os arquivos gerados futuramente

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-print('windows')
+# programa que busca as informações em html dos sites
+# import requests
+# from urllib.request import urlopen
+# pagina_html = urlopen("https://sempreupdate.com.br")
+# print(pagina_html.read())
